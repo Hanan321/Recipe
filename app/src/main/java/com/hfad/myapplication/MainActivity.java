0bc67bdf5d55c47a8eb31ae1f0d069aaeef79dfc
Hanan321 <hanela7904@students.ecpi.edu>
2023-04-04 18:41:00 -0400

    First Recipe1

--- app/src/main/java/com/hfad/myapplication/MainActivity.java
+++ app/src/main/java/com/hfad/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hfad.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Call onDisplayCafeLate() when button clicked
+    public void onDisplayCafeLate(View view){
+
+        // Use Intent to call the second activity
+        Intent intent = new Intent(this, CafeLatte.class);
+        startActivity(intent);
+
+    }
+
+    public void onDisplayScrambledEggs(View view){
+        Intent intent = new Intent(this, ScrambledEggs.class);
+        startActivity(intent);
+
+    }
+
+    public void onDisplayGardenSalad(View view){
+        Intent intent = new Intent(this,GardenSalad.class);
+        startActivity(intent);
     }
 }
